88ad2148fcc05b10e138bfbd281d303b5b2ce85b
Renafemarti <naynatoum@gmail.com>
2026-07-04 23:55:48 -0400

    fix: remove vizinhos e ativa a função grafo_escreve_info

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include "grafo.h"
 
-int main(int argc , char **argv){
-    printf("tudo ok");
+int main(int argc, char **argv) {
+    Grafo *g = grafo_ler("grafo_2.txt", REP_LISTA);
+    if (!g) {
+        printf("Erro ao ler o grafo.\n");
+        return 1;
+    }
+
+    grafo_escrever_info(g, "saida_info.txt");
+    printf("tudo ok\n");
+
+    grafo_destruir(g);
     return 0;
 }
